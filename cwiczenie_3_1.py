64e67326242c1f0c20b9575678c527700a15cf4e
Gabriel Pazdzior <gabriel.pazdzior@gmail.com>
2021-06-09 17:25:32 +0200

    initial

---
+++ cwiczenie_3_1.py
@@ -0,0 +1,13 @@
+shopping = {
+    "castorama" : ['deska', 'gwoździe', 'taśma', 'młotek'],
+    "ikea" : ['szafka', 'swieca', 'garnek'],
+    "sklep modelarski" : ['model fiata 126p']
+}
+shopping["sklep papierniczy"] = ['ryza papieru']
+all = 0
+for shop in shopping:
+    all = all + len(shopping[shop])
+    print("Idę do", shop.capitalize(), "kupuję tu następujące rzeczy:", shopping[shop])
+
+print(shopping.values())
+print("W sumie kupuję", all, "produktów.")
